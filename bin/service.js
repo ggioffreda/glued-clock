ed7548bc7ce3943ad61495071b8cb6ab5c8315dc
Giovanni Gioffreda <giovanni.gioffreda@gmail.com>
2016-10-02 20:32:16 +1300

    Add support for version monitoring.

--- bin/service.js
+++ bin/service.js
@@ -4,4 +4,4 @@ const ServiceManager = require('glued-common').ServiceManager
 const manager = new ServiceManager()
 const Clock = require('../clock').Clock
 
-manager.load(new Clock(process.env.GLUED_CLOCK_FILE || '/tmp/.glued-clock.json'))
+manager.load(new Clock(process.env.GLUED_CLOCK_FILE || '/tmp/.glued-clock.json'), require('../package.json'))
