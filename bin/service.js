093209a24a6863fe75cb668f910412af40c5dfb8
Giovanni Gioffreda <giovanni.gioffreda@gmail.com>
2016-10-02 19:58:16 +1300

    Rename main script.

--- bin/service.js
+++ bin/service.js
@@ -2,6 +2,6 @@
 
 const ServiceManager = require('glued-common').ServiceManager
 const manager = new ServiceManager()
-const Clock = require('../index').Clock
+const Clock = require('../clock').Clock
 
 manager.load(new Clock(process.env.GLUED_CLOCK_FILE || '/tmp/.glued-clock.json'))
